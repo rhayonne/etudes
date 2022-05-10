23b8a89ffc82025e0104cde9116733c923371599
Rhayonne <rhayonne@gmail.com>
2022-05-10 23:11:50 +0200

    Etudes
    Etudes nocturnes

--- exercicios/src/fundamentos/TipoString.java
+++ exercicios/src/fundamentos/TipoString.java
@@ -8,7 +8,26 @@ public class TipoString {
 		System.out.println(s.concat("!!!!"));
 		System.out.println(s + "!!!"); //quase o mesmo efeito que o concat
 		
-		System.out.println(s.startsWith("Boa")); //Verifica se o texto inicia com 
+		System.out.println(s.toLowerCase().startsWith("boa")); //Verifica se o texto inicia com BOA
+		System.out.println(s.length()); //quantos caracteres existem nesse string
+		System.out.println(s.endsWith("tarde")); //verifica se termina com tarde
+		System.out.println(s.toUpperCase().endsWith("TARDE")); //verifica se termina com TARDE e colocar maisculo
+		System.out.println(s.toLowerCase().equals("boa tarde"));//coloca em minusculo e verifica a string
+		System.out.println(s.equalsIgnoreCase("boa tarde"));
+		
+		
+		var nome = "Pedro";
+		var sobrenome = "Santos";
+		var idade = 33;
+		var salario = 12345.987;
+		
+		System.out.println("Nome: " + nome + "\nSobrenome:" + sobrenome + "\nIdade: " + idade + "\nSalario" + salario + "\n\n");
+		
+		System.out.printf("O senhor %s %s tem %d e ganha R$ %.2f \n\n",nome, sobrenome, idade, salario );
+		String frase =  String.format("O senhor %s %s tem %d e ganha R$ %.2f",nome, sobrenome, idade, salario);
+		
+		System.out.println(frase);
+		
 		
 	}
 
