97e6be47f4a349a682e2378d18ab8ffe63a0e965
Rhayonne <rhayonne@gmail.com>
2022-05-11 16:31:03 +0200

    Update TipoString.java

--- exercicios/src/fundamentos/TipoString.java
+++ exercicios/src/fundamentos/TipoString.java
@@ -22,12 +22,18 @@ public class TipoString {
 		var salario = 12345.987;
 		
 		System.out.println("Nome: " + nome + "\nSobrenome:" + sobrenome + "\nIdade: " + idade + "\nSalario" + salario + "\n\n");
+		String frase2 = String.format("Nome: " + nome + "\nSobrenome:" + sobrenome + "\nIdade: " + idade + "\nSalario" + salario + "\n\n");
+		System.out.println(frase2);
 		
 		System.out.printf("O senhor %s %s tem %d e ganha R$ %.2f \n\n",nome, sobrenome, idade, salario );
 		String frase =  String.format("O senhor %s %s tem %d e ganha R$ %.2f",nome, sobrenome, idade, salario);
 		
 		System.out.println(frase);
 		
+		System.out.println("Frase de teste".toLowerCase());
+		System.out.println("Frase de teste".contains("de"));
+		System.out.println("Frase de teste".substring(6)); //vai começar a imprimir a partir da posicao 6
+		System.out.println("Frase de teste".substring(4, 10)); // vai imprimir da posicao 4 até a 9 (a 10 nao conta)
 		
 	}
 
